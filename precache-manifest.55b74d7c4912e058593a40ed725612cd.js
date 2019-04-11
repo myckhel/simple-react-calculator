24dc9f34ad72e2d4dcb9e7b4e4cb3c96c926220d
myckhel <myckhel1@hotmail.com>
2019-04-11 10:41:46 +0100

    Updates

---
+++ precache-manifest.55b74d7c4912e058593a40ed725612cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc2f8c7b22c570d0dbaa",
+    "url": "/static/js/main.35bc6284.chunk.js"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "dc2f8c7b22c570d0dbaa",
+    "url": "/static/css/main.939a869e.chunk.css"
+  },
+  {
+    "revision": "eba289b111c89f89d5a11ce19e9b2c78",
+    "url": "/index.html"
+  }
+];
